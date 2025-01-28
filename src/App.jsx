c7e0ec922f314d25244e8f286f2937feb4eb6dc5
24dimitris <24dimitris@gmail.com>
2025-01-28 21:13:38 +0000

    Deployed to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
      <Navbar/>
-     <BrowserRouter>
+     <BrowserRouter basename='/coursera-capstone'>
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
